650066e5230f21f937dd1602e90fc5c3f285291e
gilron31 <gilron31@gmail.com>
2021-03-25 15:56:33 +0200

    added documentation of first alleged success

---
+++ experiments/scheme_3_scratchpad/scratchpad_main.m
@@ -0,0 +1,12 @@
+%%
+optical_setup.pump_on = 0;
+optical_setup.prob_on = 1;
+optical_setup.pump_detune = 200e-3;
+optical_setup.prob_detune = -12e-3;
+optical_setup.pump_ampere = 123;
+optical_setup.prob_ampere = 102;
+optical_setup.pump_power = 'max (probably around 500';
+optical_setup.prob_power = 5.1e-3;
+
+%%
+
